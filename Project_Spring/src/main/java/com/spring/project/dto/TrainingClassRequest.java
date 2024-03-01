9b24c121bd334a8bc29ace4a5275a56c3cf80203
MergeaSergiu <mergeasergiu@gmail.com>
2024-03-01 23:15:37 +0200

    Added TrainingClassRequest class

---
+++ Project_Spring/src/main/java/com/spring/project/dto/TrainingClassRequest.java
@@ -0,0 +1,21 @@
+package com.spring.project.dto;
+
+
+import lombok.*;
+
+@Data
+@Builder
+@Getter
+@Setter
+public class TrainingClassRequest {
+
+    private String className;
+
+    private int duration;
+
+    private String intensity;
+
+    private String localDate;
+
+    private Integer trainerId;
+}
