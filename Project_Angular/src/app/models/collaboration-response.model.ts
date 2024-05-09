8f20c513fc912472af1b4dfae4605d45ab5cd73a
MergeaSergiu <mergeasergiu@gmail.com>
2024-05-09 09:00:38 +0300

    Update TrainerCollaborationResponse

--- Project_Angular/src/app/models/collaboration-response.model.ts
+++ Project_Angular/src/app/models/collaboration-response.model.ts
@@ -2,6 +2,7 @@ export interface CollaborationResponse{
     collaborationId: number;
     firstName: string;
     lastName: string;
+    email: string;
     collaborationStatus: string;
     startTime: Date;
     endTime: Date;
